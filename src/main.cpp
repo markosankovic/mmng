f935dfbf55f107c6226aa9ea392fe70396757fa5
Marko Sanković <sankovic.marko@gmail.com>
2025-01-09 09:54:15 +0100

    Add uwebsockets

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <fmt/core.h>
+#include <uwebsockets/App.h>
 
-int main() {
-    fmt::print("Hello World!\n");
+int main()
+{
+    uWS::App()
+        .get("/", [](auto *res, auto *req)
+             {
+            res->writeHeader("Content-Type", "text/html; charset=utf-8");
+            res->end("<h1>Welcome to uWebSockets HTTP Server</h1>"); })
+        .listen(9000, [](auto *token) {
+            if (token) {
+                std::cout << "Server is listening on port 9000" << std::endl;
+            } else {
+                std::cerr << "Failed to start server!" << std::endl;
+            }
+        })
+        .run();
+    fmt::print("uWebSockets HTTP Server Stopped!\n");
     return 0;
 }
